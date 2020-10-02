e71220d1b8d46e2de92a3f8f6136a2b76340887e
Bernardo <bernardo.carvalho@tecnico.ulisboa.pt>
2020-10-02 21:26:58 +0100

    Initial Import

---
+++ heltec-lora-v2/src/main.cpp
@@ -0,0 +1,276 @@
+/*  Board Heltec Wifi Lora 32 (V2)
+ *  Heltec Automation I2C scanner example (also it's a basic example how to use I2C1)
+ *
+ * ESP32 have two I2C (I2C0 and I2C1) bus
+ *
+ * OLED is connected to I2C0, so if scan with Wire (I2C0), the return address should be 0x3C.
+ *
+ * If you need scan other device address in I2C1...
+ *		- Comment all Wire.***() codes;
+ * 		- Uncomment all Wire1.***() codes;
+ *
+ * I2C scan example and I2C0
+ *
+ * HelTec AutoMation, Chengdu, China
+ * 成都惠利特自动化科技有限公司
+ * www.heltec.org
+ *
+ * this project also realess in GitHub:
+ * https://github.com/HelTecAutomation/Heltec_ESP32
+ * */
+
+//I2C device BME680 found at address 0x77  !
+
+#include "Arduino.h"
+#include "heltec.h"
+#include "bsec.h"
+
+#include <WiFi.h>
+
+const char* ssid     = "Vodafone-05C121";
+const char* password = "dpsjKk34U2";
+
+
+#define BME680_I2C_ADDR 0x77
+
+// Helper functions declarations
+void checkIaqSensorStatus(void);
+void errLeds(void);
+
+void read_bme680(void);
+
+Bsec iaqSensor;
+String output;
+
+void connect_wifi()
+{
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+
+
+void scan_I2C()
+{
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ )
+  {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+//    Wire1.beginTransmission(address);
+//    error = Wire1.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16)
+      Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4)
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16)
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }
+  }
+  if (nDevices == 0)
+  Serial.println("No I2C devices found\n");
+  else
+  Serial.println("done\n");
+
+}
+void setup()
+{
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+	Heltec.begin(true, false, true);
+	Wire.begin(SDA_OLED, SCL_OLED); //Scan OLED's I2C address via I2C0
+	//Wire1.begin(SDA, SCL);        //If there have other device on I2C1, scan the device address via I2C1
+
+   Serial.begin(115200); //Not needed
+   while(!Serial);    // time to get serial running
+   Serial.println(F("BME680 test Init"));
+
+  iaqSensor.begin(BME680_I2C_ADDR, Wire);
+  output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
+  Serial.println(output);
+  checkIaqSensorStatus();
+  Serial.println("BME680 0x77 OK");
+  bsec_virtual_sensor_t sensorList[10] = {
+    BSEC_OUTPUT_RAW_TEMPERATURE,
+    BSEC_OUTPUT_RAW_PRESSURE,
+    BSEC_OUTPUT_RAW_HUMIDITY,
+    BSEC_OUTPUT_RAW_GAS,
+    BSEC_OUTPUT_IAQ,
+    BSEC_OUTPUT_STATIC_IAQ,
+    BSEC_OUTPUT_CO2_EQUIVALENT,
+    BSEC_OUTPUT_BREATH_VOC_EQUIVALENT,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+    BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+  };
+
+  iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
+  checkIaqSensorStatus();
+   Heltec.display->flipScreenVertically();
+  Heltec.display->setFont(ArialMT_Plain_10);
+// clear the display
+  Heltec.display->clear();
+   //display.setTextColor(WHITE, BLACK);
+
+  Heltec.display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  Heltec.display->drawString(0, 0, "Starting BME ");
+
+  //Heltec.display->drawString(10, 128, String(millis()));
+  // write the buffer to the display
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->drawString(0, 10, "Temp VOC IAQ");
+
+  // The coordinates define the center of the text
+  //Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
+  //Heltec.display->drawString(64, 22, "Center aligned (64,22)");
+
+  // The coordinates define the right end of the text
+  Heltec.display->setTextAlignment(TEXT_ALIGN_RIGHT);
+  Heltec.display->drawString(128, 33, "Right aligned (128,33)");
+
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+  Heltec.display->drawString(0, 44, "Temp:");
+
+
+  Heltec.display->display();
+  //connect_wifi();
+
+}
+
+void loop2()
+{
+  //scan_I2C();
+  void read_bme680(void);
+}
+
+void print_bme680(unsigned long time){
+    output = String(time);
+    output += ", " + String(iaqSensor.rawTemperature);
+    output += ", " + String(iaqSensor.pressure);
+    output += ", " + String(iaqSensor.rawHumidity);
+    output += ", " + String(iaqSensor.gasResistance);
+    output += ", " + String(iaqSensor.iaq);
+    output += ", " + String(iaqSensor.iaqAccuracy);
+    output += ", " + String(iaqSensor.temperature);
+    output += ", " + String(iaqSensor.humidity);
+    output += ", " + String(iaqSensor.staticIaq);
+    output += ", " + String(iaqSensor.co2Equivalent);
+    output += ", " + String(iaqSensor.breathVocEquivalent);
+    Serial.println(output);
+  
+  }
+void loop(void)
+{
+  unsigned long time_trigger = millis();
+  if (iaqSensor.run()) { // If new data is available
+    print_bme680(time_trigger);
+    Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
+    Heltec.display->drawString(20, 22, "              ");
+    Heltec.display->drawString(20, 22, String(iaqSensor.temperature) );
+    Heltec.display->drawString(60, 22, String(iaqSensor.breathVocEquivalent) );
+
+    Heltec.display->display();
+
+
+  } else {
+    checkIaqSensorStatus();
+  }
+}
+
+void read_bme680(void)
+{
+  unsigned long time_trigger = millis();
+  if (iaqSensor.run()) { // If new data is available
+    output = String(time_trigger);
+    output += ", " + String(iaqSensor.rawTemperature);
+    output += ", " + String(iaqSensor.pressure);
+    output += ", " + String(iaqSensor.rawHumidity);
+    output += ", " + String(iaqSensor.gasResistance);
+    output += ", " + String(iaqSensor.iaq);
+    output += ", " + String(iaqSensor.iaqAccuracy);
+    output += ", " + String(iaqSensor.temperature);
+    output += ", " + String(iaqSensor.humidity);
+    output += ", " + String(iaqSensor.staticIaq);
+    output += ", " + String(iaqSensor.co2Equivalent);
+    output += ", " + String(iaqSensor.breathVocEquivalent);
+    Serial.println(output);
+    Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
+    Heltec.display->drawString(64, 44, String(iaqSensor.temperature) + 'o');
+    // write the buffer to the display
+    Heltec.display->display();
+    errLeds();
+
+  } else {
+    checkIaqSensorStatus();
+  }
+}
+
+// Helper function definitions
+void checkIaqSensorStatus(void)
+{
+  if (iaqSensor.status != BSEC_OK) {
+    if (iaqSensor.status < BSEC_OK) {
+      output = "BSEC error code : " + String(iaqSensor.status);
+      Serial.println(output);
+  //    for (;;)
+  //      errLeds(); /* Halt in case of failure */
+    } else {
+      output = "BSEC warning code : " + String(iaqSensor.status);
+      Serial.println(output);
+    }
+  }
+
+  if (iaqSensor.bme680Status != BME680_OK) {
+    if (iaqSensor.bme680Status < BME680_OK) {
+      output = "BME680 error code : " + String(iaqSensor.bme680Status);
+      Serial.println(output);
+ //     for (;;)
+ //       errLeds(); /* Halt in case of failure */
+    } else {
+      output = "BME680 warning code : " + String(iaqSensor.bme680Status);
+      Serial.println(output);
+    }
+  }
+
+}
+
+void errLeds(void)
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(100);
+}
